cf3555771ad507f133306ba9b5177cbf82152ec5
ninjaneural <127111336+ninjaneural@users.noreply.github.com>
2023-05-11 10:55:21 +0900

    수정

--- scripts/ddetailer.py
+++ scripts/ddetailer.py
@@ -46,8 +46,8 @@ def startup():
     if not is_installed("mmdet"):
         python = sys.executable
         run(f'"{python}" -m pip install -U openmim', desc="Installing openmim", errdesc="Couldn't install openmim")
-        run(f'"{python}" -m mim install mmcv>=2.0.0', desc=f"Installing mmcv", errdesc=f"Couldn't install mmcv")
-        run(f'"{python}" -m pip install mmdet>=3.0.0', desc=f"Installing mmdet", errdesc=f"Couldn't install mmdet")
+        run(f'"{python}" -m mim install mmcv-full', desc=f"Installing mmcv-full", errdesc=f"Couldn't install mmcv-full")
+        run(f'"{python}" -m pip install mmdet==2.28.2', desc=f"Installing mmdet", errdesc=f"Couldn't install mmdet")
 
     if (len(list_models(dd_models_path)) == 0):
         print("No detection models found, downloading...")
